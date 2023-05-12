8855e3c4764b761e777a5dde1ded1295b33556ec
rodrigo-rac2 <rodrigo-rac2@users.noreply.github.com>
2023-05-12 19:21:10 +0000

    deploy: 3a32b9335444fede214b2aa2b75fe45fd150f516

---
+++ precache-manifest.3ea257b29a128c9b9698ed19f80a5504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bb53a305cf31bad4b1679410ca0e58",
+    "url": "/appointments-react/index.html"
+  },
+  {
+    "revision": "dbe6b9ae024b4ef47fc7",
+    "url": "/appointments-react/static/css/main.451647fe.chunk.css"
+  },
+  {
+    "revision": "9dfaa24cf8fc68d2e090",
+    "url": "/appointments-react/static/js/2.49032cef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/appointments-react/static/js/2.49032cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe6b9ae024b4ef47fc7",
+    "url": "/appointments-react/static/js/main.72d19881.chunk.js"
+  },
+  {
+    "revision": "d4daff7ab111453c8767",
+    "url": "/appointments-react/static/js/runtime-main.159e76d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e6802d9fd5af98389e0ccb56dd4a80e.js"
+  "/appointments-react/precache-manifest.3ea257b29a128c9b9698ed19f80a5504.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/appointments-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
