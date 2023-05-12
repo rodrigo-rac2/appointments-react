5e5df57c16101f40ad1da8228c5ab56f9cd10c9c
rodrigo-rac2 <rodrigo-rac2@users.noreply.github.com>
2023-05-12 17:20:04 +0000

    deploy: 6b8773aa3a7889711c8c705438f901cb0577ddcb

---
+++ precache-manifest.213ac55fd654a915b8de526c426dc2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5129973fa8407428da1fe1ce98feeea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b78dc29b8a0cd626d8",
+    "url": "/static/css/main.451647fe.chunk.css"
+  },
+  {
+    "revision": "9dfaa24cf8fc68d2e090",
+    "url": "/static/js/2.49032cef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.49032cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b78dc29b8a0cd626d8",
+    "url": "/static/js/main.5330399f.chunk.js"
+  },
+  {
+    "revision": "d123a1434e472fc643e7",
+    "url": "/static/js/runtime-main.7b38d805.js"
+  }
+]);
